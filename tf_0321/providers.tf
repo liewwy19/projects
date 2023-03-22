18816c6962e36a6de5a71274714342f02a2ec58d
liewwy19@vscode7490 <liewwy19+vscode@gmail.com>
2023-03-22 08:36:34 +0800

    add shared config to providers

--- tf_0321/providers.tf
+++ tf_0321/providers.tf
@@ -8,6 +8,7 @@ terraform {
 
 provider "aws" {
   region                   = "ap-southeast-1"
+  shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "vscode"
 }
